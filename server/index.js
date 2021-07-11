038036e73a4f818790854e1341a43abd09a9ffb2
Sashmita Raghav <sashmitaraghav2505@gmail.com>
2021-07-11 23:41:43 +0530

    Update files

--- server/index.js
+++ server/index.js
@@ -59,6 +59,15 @@ io.on("connect", (socket) => {
     callback();
   })
 
+  socket.on('sendNotes', (noteFromPad, callback) => {
+    console.log("received notes", noteFromPad);
+    const user = getUser(socket.id);
+
+    socket.broadcast.to(user.room).emit('notes', {text: noteFromPad})
+
+    callback();
+  })
+
   socket.on("sending signal", payload => {
         //console.log("sending signal ",payload.userToSignal)
         //socket.broadcast.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
@@ -87,4 +96,4 @@ io.on("connect", (socket) => {
 });
 
 
-server.listen(process.env.PORT || 5001, () => console.log(`Server has started.`));
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
